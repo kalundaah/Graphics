1711b7c95fd910cbbb47e77354a0a540b5b09032
ruai-dak <98888167+ruai-dak@users.noreply.github.com>
2024-01-17 11:26:53 +0300

    hold

--- Kalundas version/kalunda.m
+++ Kalundas version/kalunda.m
@@ -17,7 +17,12 @@ van = vrnode(world,'Van');
 man.translation = [6.10,-3,-27];
 boy.translation = [8.37,-5,-37.84];
 
+location = 'decos.png';
+deco = imread(location);
+imshow(deco);
 
+children = get(world,'Nodes');
+disp(children)
 
 camera.position = [-3.90, -3.0, -27.84];
 camera.orientation =[0,0.9,0,-0.80];
@@ -29,7 +34,6 @@ numSteps = 150;
 
 %Loop to gradually change the x position of camera
 for step = 1:numSteps
-    disp (camera.position(1));
     value = camera.position(1);
     value = value + 0.05;
     camera.position = [value,camera.position(2),camera.position(3)];
@@ -43,7 +47,6 @@ vrdrawnow;
 numSteps = 200;
 
 for step = 1:numSteps
-    disp (camera.position(1));
     value = camera.position(1);
     value = value + 0.05;
     camera.position = [value,camera.position(2),camera.position(3)];
@@ -58,7 +61,6 @@ vrdrawnow;
 numSteps = 200;
 
 for step = 1:numSteps
-    disp (camera.position(1));
     value = camera.position(1);
     value = value + 0.05;
     camera.position = [value,camera.position(2),camera.position(3)];
@@ -70,7 +72,6 @@ camera.orientation =[0,0.2,0,-0.75];
 vrdrawnow;
 
 for step = 1:numSteps
-    disp (camera.position(2));
     value = camera.position(2);
     value = value - 0.01;
     camera.position = [camera.position(1),value,camera.position(3)];
